9a6f26ccacc5945e9e7db1a24f1eb1690f9775a3
jwwasylenko <jwwasy@gmail.com>
2025-02-13 20:13:59 -0700

    Add files via upload

---
+++ triptovancouver.js
@@ -0,0 +1,54 @@
+february15th.addEventListener('click',function(){
+    console.log("Day being changed");
+    document.getElementById("banner").src="images/RussianAvantGarde000.jpg";
+    document.getElementById("date").innerHTML="February 15th 2025";
+    console.log("Artwork being changed");
+    document.getElementById("anecdotes").innerHTML="This is the day I arrive at 6:00AM. Waking up early and going to the airport.";
+    console.log("anecdotes changing");
+})
+
+february16th.addEventListener('click',function(){
+    console.log("Day being changed");
+    document.getElementById("banner").src="images/Phong Nai000.jpg";
+    document.getElementById("date").innerHTML="February 16th 2025";
+    console.log("Artwork being changed");
+    document.getElementById("anecdotes").innerHTML="This is my first full day in Vancouver.";
+    console.log("anecdotes changing");
+
+})
+
+february17th.addEventListener('click',function(){
+    console.log("Day being changed");
+    document.getElementById("banner").src="images/Watercolours000.jpg";
+    document.getElementById("date").innerHTML="February 17th 2025";
+    console.log("Artwork being changed");
+    document.getElementById("anecdotes").innerHTML="This will be my second full day in Vancouver. Family Day. Canada plays Finland at 11:00AM in the 4 Nations Faceoff.";
+
+})
+
+february18th.addEventListener('click',function(){
+    console.log("Day being changed");
+    document.getElementById("banner").src="images/Nocturne(Brainstorming2)000.jpg";
+    document.getElementById("date").innerHTML="February 18th 2025";
+    console.log("Artwork being changed");
+    document.getElementById("anecdotes").innerHTML="Tuesday.";
+
+})
+
+february19th.addEventListener('click',function(){
+    console.log("Day being changed");
+    document.getElementById("banner").src="images/Nocturne(ArtNouveau)000.jpg";
+    document.getElementById("date").innerHTML="February 19th 2025";
+    console.log("Artwork being changed");
+    document.getElementById("anecdotes").innerHTML="Wednesday. Last full day in Vanocuver.";
+
+})
+
+february20th.addEventListener('click',function(){
+    console.log("Day being changed");
+    document.getElementById("banner").src="images/Nocturne(Brainstorming)000.jpg";
+    document.getElementById("date").innerHTML="February 20th 2025";
+    console.log("Artwork being changed");
+    document.getElementById("anecdotes").innerHTML="This will be my last day in Vancouver. I fly home around 4:00PM.";
+
+})
