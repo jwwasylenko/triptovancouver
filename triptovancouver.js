f1e64b432160eb000d34e20586b2d5f58f686ea0
jwwasylenko <jwwasy@gmail.com>
2025-03-03 12:36:31 -0700

    Add files via upload

--- triptovancouver.js
+++ triptovancouver.js
@@ -1,15 +1,15 @@
 february15th.addEventListener('click',function(){
     console.log("Day being changed");
-    document.getElementById("banner").src="images/RussianAvantGarde000.jpg";
+    document.getElementById("banner").src="images/February16th2025.jpg";
     document.getElementById("date").innerHTML="February 15th 2025";
     console.log("Artwork being changed");
-    document.getElementById("anecdotes").innerHTML="This is the day I arrive at 6:00AM. Waking up early and going to the airport.";
+    document.getElementById("anecdotes").innerHTML="This is the day I arrive at 6:00AM. Waking up early and going to the airport. Here is an update, I am coding on the plane. I flew Flair airlines for the first time and had not issues with baggage. I wonder what I will do in Vancouver. I think it will be like Montreal without Sam. We saw the Canada vs USA game.";
     console.log("anecdotes changing");
 })
 
 february16th.addEventListener('click',function(){
     console.log("Day being changed");
-    document.getElementById("banner").src="images/Phong Nai000.jpg";
+    document.getElementById("banner").src="images/February16th2025.jpg";
     document.getElementById("date").innerHTML="February 16th 2025";
     console.log("Artwork being changed");
     document.getElementById("anecdotes").innerHTML="This is my first full day in Vancouver.";
@@ -19,7 +19,7 @@ february16th.addEventListener('click',function(){
 
 february17th.addEventListener('click',function(){
     console.log("Day being changed");
-    document.getElementById("banner").src="images/Watercolours000.jpg";
+    document.getElementById("banner").src="images/Walkingonshoreline.jpg";
     document.getElementById("date").innerHTML="February 17th 2025";
     console.log("Artwork being changed");
     document.getElementById("anecdotes").innerHTML="This will be my second full day in Vancouver. Family Day. Canada plays Finland at 11:00AM in the 4 Nations Faceoff.";
